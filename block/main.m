75e3fc881b015cf27208963d11b2775270ff5edb
apple <apple@appledeMacBook-Pro.local>
2015-04-14 09:51:05 +0800

    Initial Commit

---
+++ block/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  block
+//
+//  Created by apple on 15/4/14.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
